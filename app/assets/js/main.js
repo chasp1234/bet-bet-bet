fef5aff237657875f19a2ea26a17fd5a9fd990de
Ubuntu <ubuntu@ip-172-31-86-239.ec2.internal>
2026-03-16 13:02:42 +0000

    Replace legacy static app with Bet Bet Bet MVP build

--- app/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.topnav a').forEach((link) => {
-  link.addEventListener('click', (event) => {
-    event.preventDefault();
-    const id = link.getAttribute('href');
-    const target = id ? document.querySelector(id) : null;
-    target?.scrollIntoView({ behavior: 'smooth', block: 'start' });
-  });
-});
